8c4f9b5f2168a3a9c19f868a70147ee5c6be4da5
Polzovat123 <42420479+Polzovat123@users.noreply.github.com>
2020-09-22 01:09:09 +0500

    Create Tables_Pifagor.cpp

---
+++ Лекция_1/Tables_Pifagor.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int s;
+    scanf("%d", &s);
+    for(int i=0;i<=((s+1)*6-15)/2+1;i++){
+        printf("-");
+    }
+    printf("<Table Pifagor>");
+    for(int i=0;i<=((s+1)*6-15)/2;i++){
+        printf("-");
+    }
+    printf("\n|");
+    //name string
+    for(int i=0;i<=s;i++){
+        if(i!=0){printf ("|% 4d|", i-1);}else{
+            printf("|----|");
+        }
+    }
+    printf("|\n");
+    for(int i=0;i<s;i++){
+        printf("|");
+        //write column
+        printf("|% 4d|",i);
+        for(int j=0;j<s;j++){
+            printf ("|% 4d|", i*j);
+        }
+        printf("|");
+        printf("\n");
+    }
+    for(int i=0;i<=(s+1)*6+1;i++){
+        printf("-");
+    }
+
+    //printf ("% 4d", 1);
+    //printf ("1\t2\n123\t2\n");
+    return 0;
+}
