758c5bc471140a0d2848a695fbc0ebce65c03638
Polzovat123 <42420479+Polzovat123@users.noreply.github.com>
2020-12-15 22:41:22 +0500

    Update main.cpp

--- Лекция_6/main.cpp
+++ Лекция_6/main.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
 
+using namespaxe std;
+
+class Matrix
+{
+  public:
+  double *base;
+  double determenant;
+  
+  Matrix(double *arr, int x, int y){
+    base =  new long double[100][100];
+    for(int i=0; i<x; i++){
+      for(int j=0; j<y; j++){
+	base[i][j] = arr[i][j];
+      }
+    }
+  };
+
+  void get_determenant(){
+    
+    return;
+  }
+  
+  protected:
+  /*список средств, доступных при наследовании*/
+};
+
+void 
+
+
+int main()
+{
+  
+  return 0;
+}
